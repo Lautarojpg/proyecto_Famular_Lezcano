2721001a95f0ab29d5c3aadc193d610362e8e8a1
antonelafamular <famularantonela@gmail.com>
2025-09-15 18:03:31 -0300

    cambio de color login

--- FormLogin.Designer.cs
+++ FormLogin.Designer.cs
@@ -38,40 +38,43 @@
             // LUsuario
             // 
             LUsuario.AutoSize = true;
-            LUsuario.Location = new Point(110, 9);
+            LUsuario.Location = new Point(126, 12);
             LUsuario.Name = "LUsuario";
-            LUsuario.Size = new Size(47, 15);
+            LUsuario.Size = new Size(59, 20);
             LUsuario.TabIndex = 0;
             LUsuario.Text = "Usuario";
             // 
             // LPassword
             // 
             LPassword.AutoSize = true;
-            LPassword.Location = new Point(99, 80);
+            LPassword.Location = new Point(113, 107);
             LPassword.Name = "LPassword";
-            LPassword.Size = new Size(67, 15);
+            LPassword.Size = new Size(83, 20);
             LPassword.TabIndex = 1;
             LPassword.Text = "Contraseña";
             // 
             // TUsuario
             // 
-            TUsuario.Location = new Point(84, 38);
+            TUsuario.Location = new Point(96, 51);
+            TUsuario.Margin = new Padding(3, 4, 3, 4);
             TUsuario.Name = "TUsuario";
-            TUsuario.Size = new Size(100, 23);
+            TUsuario.Size = new Size(114, 27);
             TUsuario.TabIndex = 2;
             // 
             // TPassword
             // 
-            TPassword.Location = new Point(84, 113);
+            TPassword.Location = new Point(96, 151);
+            TPassword.Margin = new Padding(3, 4, 3, 4);
             TPassword.Name = "TPassword";
-            TPassword.Size = new Size(100, 23);
+            TPassword.Size = new Size(114, 27);
             TPassword.TabIndex = 3;
             // 
             // BLogin
             // 
-            BLogin.Location = new Point(99, 151);
+            BLogin.Location = new Point(113, 201);
+            BLogin.Margin = new Padding(3, 4, 3, 4);
             BLogin.Name = "BLogin";
-            BLogin.Size = new Size(75, 23);
+            BLogin.Size = new Size(86, 31);
             BLogin.TabIndex = 4;
             BLogin.Text = "Ingresar";
             BLogin.UseVisualStyleBackColor = true;
@@ -79,14 +82,16 @@
             // 
             // FormLogin
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(287, 195);
+            BackColor = Color.FromArgb(255, 224, 192);
+            ClientSize = new Size(328, 260);
             Controls.Add(BLogin);
             Controls.Add(TPassword);
             Controls.Add(TUsuario);
             Controls.Add(LPassword);
             Controls.Add(LUsuario);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FormLogin";
             Text = "FormLogin";
             ResumeLayout(false);
